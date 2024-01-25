b95304403f74bdc4e8b5343991123fe11df41361
Afsheen <afsheen@duck.com>
2024-01-24 21:07:50 -0600

    got TxnControllerIT to test findAll

--- src/test/java/com/afsheen/portfoliotracker/controller/TxnControllerIT.java
+++ src/test/java/com/afsheen/portfoliotracker/controller/TxnControllerIT.java
@@ -1,47 +1,77 @@
 package com.afsheen.portfoliotracker.controller;
 
-
+import com.afsheen.portfoliotracker.repository.TxnRepository;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Testing with TestRestTemplate and @Testcontainers (image mysql:8.0-debian)
  */
+@IfProfileValue(name="spring.profiles.active", value="integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 // activate automatic startup and stop of containers
 @Testcontainers
 // JPA drop and create table, good for testing
 @TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=create-drop"})
 public class TxnControllerIT {
-    
 
-    // static, all tests share this postgres container
+    // static, all tests share this mysql container
     @Container
     @ServiceConnection
-    static MySQLContainer<?> postgres = new MySQLContainer<>(
-        "mysql:8.0-debian"
-    );
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(
+        DockerImageName.parse("mysql:8.0.36")
+                .asCompatibleSubstituteFor("mysql"))
+            .withDatabaseName("portfolio")
+            .withUsername("afsdb")
+            .withPassword("afsdb");
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private TxnController txnController;
+
+    @Autowired
+    private TxnRepository txnRepository;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.port = port;
+        txnRepository.deleteAll();
+    }
+
+    @Test
+    void testSetup() {
+        assertTrue(mySQLContainer.isRunning());
+    }
 
     @Test
     void testFindAll() {
-        
+        Response response = when()
+                .request("GET", "/txns")
+                .then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .extract()
+                .response();
+
+        assertNotNull(response);
     }
 }
