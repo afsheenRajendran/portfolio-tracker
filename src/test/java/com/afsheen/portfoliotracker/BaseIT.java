8969e0369cd231ba3501e66c4e220c0bba187d6d
Afsheen <afsheen@duck.com>
2024-02-27 21:08:44 -0600

    updated PortfolioTrackerApplicationTests

--- src/test/java/com/afsheen/portfoliotracker/BaseIT.java
+++ src/test/java/com/afsheen/portfoliotracker/BaseIT.java
@@ -1,24 +1,30 @@
 package com.afsheen.portfoliotracker;
 
-
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @ContextConfiguration(initializers = BaseIT.DataSourceInitializer.class)
 public abstract class BaseIT {
-    @Container
-    private static final MySQLContainer<?> database = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.35")
-            .asCompatibleSubstituteFor("mysql"))
-            .withDatabaseName("portfolio")
-            .withUsername("afs")
-            .withPassword("afs");
+
+    private static final MySQLContainer<?> database;
+
+    static {
+        // see https://java.testcontainers.org/test_framework_integration/manual_lifecycle_control/#singleton-containers
+
+        database = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.35")
+                .asCompatibleSubstituteFor("mysql"))
+                .withDatabaseName("portfolio")
+                .withUsername("afs")
+                .withPassword("afs");
+
+        database.start();
+    }
 
     public static class DataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
--- src/test/java/com/afsheen/portfoliotracker/PortfolioTrackerApplicationTests.java
+++ src/test/java/com/afsheen/portfoliotracker/PortfolioTrackerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.afsheen.portfoliotracker;
 
+import com.afsheen.portfoliotracker.service.TxnService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-class PortfolioTrackerApplicationTests {
+class PortfolioTrackerApplicationTests extends BaseIT {
+
+	@Autowired
+	private TxnService txnService;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(txnService);
 	}
 
 }
